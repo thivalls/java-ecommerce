946785a4c24172c6c8d6c96a5936fd06f4b0ebe6
valls <olavalls@gmail.com>
2021-11-19 23:22:32 -0300

    Criando projeto inicial

---
+++ src/main/java/com/br/javaecommerce/JavaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.br.javaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/javaecommerce/JavaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.javaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
