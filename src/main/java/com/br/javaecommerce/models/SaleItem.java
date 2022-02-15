48ecb6f47fbe8652b33c071af346f2aa0da08b47
valls <olavalls@gmail.com>
2022-02-14 23:05:22 -0300

    Criando entidade SaleItem

---
+++ src/main/java/com/br/javaecommerce/models/SaleItem.java
@@ -0,0 +1,111 @@
+package com.br.javaecommerce.models;
+
+import java.util.Objects;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sale_items")
+public class SaleItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "sale_item_product_fk"))
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "sale_item_order_fk"))
+    private Order order;
+
+    private Double quantity;
+
+
+    public SaleItem() {
+    }
+
+    public SaleItem(long id, Product product, Order order, Double quantity) {
+        this.id = id;
+        this.product = product;
+        this.order = order;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return this.order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Double getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public SaleItem id(long id) {
+        setId(id);
+        return this;
+    }
+
+    public SaleItem product(Product product) {
+        setProduct(product);
+        return this;
+    }
+
+    public SaleItem order(Order order) {
+        setOrder(order);
+        return this;
+    }
+
+    public SaleItem quantity(Double quantity) {
+        setQuantity(quantity);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof SaleItem)) {
+            return false;
+        }
+        SaleItem saleItem = (SaleItem) o;
+        return id == saleItem.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
